1478c3b9dff81769478dfce5450bbf418f65b3cd
Natan Brito <natanismaelbrito@gmail.com>
2022-08-20 16:01:20 -0300

    make back-end and seed run

--- back-end/prisma/seed.ts
+++ back-end/prisma/seed.ts
@@ -6,18 +6,109 @@ async function main() {
   const students = [
     { name: "Diego Pinho" },
     { name: "Bruna Hamori" },
-    { name: "Maria Fernanda" }
-  ]
+    { name: "Maria Fernanda" },
+    { name: "Lucax da t7" },
+    { name: "Thalles da Branch" },
+    { name: "loro jose" },
+    { name: "Gabriel VIMana" },
+    { name: "Caio Calvo" },
+    { name: "Matheus chili" },
+    { name: "Natan maladoo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+    { name: "Thay" },
+    { name: "Thay de Novo" },
+  ];
 
   await prisma.student.createMany({
     data: students,
-    skipDuplicates: true
+    skipDuplicates: true,
   });
 }
 
-main().catch(e => {
-  console.log(e);
-  process.exit(1);
-}).finally(async () => {
-  await prisma.$disconnect();
-})
+main()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
